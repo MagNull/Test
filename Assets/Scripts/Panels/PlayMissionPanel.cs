bca03a12352616806ceeadc3b18d9dd4dd848346
MagNull <pathfo@yandex.ru>
2023-10-15 02:41:29 +0500

    Fix play panel sides

--- Assets/Scripts/Panels/PlayMissionPanel.cs
+++ Assets/Scripts/Panels/PlayMissionPanel.cs
@@ -18,8 +18,8 @@ namespace Panels
         protected override void OnActivate(IReadOnlyMission mission)
         {
             _descriptionText.text = mission.BaseData.MainText;
-            _playerSideText.text = string.Join(", ", mission.BaseData.PlayerFractions.Select(f => f.ToRussianName()));
-            _enemySideText.text = string.Join(", ", mission.BaseData.EnemyFractions.Select(f => f.ToRussianName()));
+            _playerSideText.text = string.Join(", ", mission.CurrentPlayerFractions.Select(f => f.ToRussianName()));
+            _enemySideText.text = string.Join(", ", mission.CurrentEnemyFractions.Select(f => f.ToRussianName()));
         }
     }
 }
